a0690841021dabe2b518caa36f1b3f0a337e22e8
EmitTheFortniteGod <147779921+EmitTheFortniteGod@users.noreply.github.com>
2023-11-16 11:50:11 +1100

    Update 17-TemperatureConversion.cs

--- 17-TemperatureConversion/17-TemperatureConversion.cs
+++ 17-TemperatureConversion/17-TemperatureConversion.cs
@@ -76,6 +76,84 @@ namespace ProgrammingExercisesIST
         }
     }
 }
+static void Main(string[] args)
+{
+
+
+
+
+    // Write your code here
+
+    MainMenu();
+
+
+    // Wait for key press before exiting
+    WaitForKeyPress();
+}
+
+// Create your functions here
+
+
+
+// Asks the user to enter a distance in the unit specified
+private static double GetDistance(string unit)
+{
+    Console.Write($"Enter distance (in {unit}) to convert: ");
+    double distance = int.Parse(Console.ReadLine());
+    return distance;
+}
+
+
+
+
+// Shows the menu options to the user
+private static void MainMenu()
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+    Console.Clear();
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. Kilometres to Miles");
+    Console.WriteLine("2. Miles to Kilometres");
+    string choice = Console.ReadLine();
+
+
+
+
+
+    if (choice == "1")
+    {
+        Console.Write("How many kilometres? ");
+        double km = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"{km} kilometers to miles = {km * 0.621371}");
+    }
+
+    else if (choice == "2")
+    {
+        Console.Write("How many miles? ");
+        double miles = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"{miles} miles to kilometers = {miles * 1.60934}");
+    }
+
+}
+
+// Waits for the user to press any key
+private static void WaitForKeyPress()
+{
+    Console.WriteLine();
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+// Displays an error message in red text
+private static void ErrorMessage(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+    }
+}
 
 /*
 ===============================================================================
